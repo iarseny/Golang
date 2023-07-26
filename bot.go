43f755232fe26b9e5333e0fa6cd5caffc0ccf39e
iarseny <122807698+iarseny@users.noreply.github.com>
2023-07-26 14:24:04 +0000

    homework

--- bot.go
+++ bot.go
@@ -1,8 +1,29 @@
 package main
 
-import ("fmt" 
-"net/http")
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello, world!");
+	bot, err := tgbotapi.NewBotAPI("5327315256:AAGvkuzryn2dFx0NmYgRpajAWpjAjS_HoNs")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message.IsCommand() {
+			var msg = tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			msg.Text = "Binance"
+			bot.Send(msg);
+		}
+	}
 }
